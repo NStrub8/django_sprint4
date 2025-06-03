8c79e4fc615609aafb5c199724d36b01754d2655
Nikita Strubalin <nikita.strubalin@yandex.ru>
2025-06-03 14:05:53 +0300

    6

--- blogicum/blogicum/urls.py
+++ blogicum/blogicum/urls.py
@@ -17,7 +17,6 @@ Including another URLconf
     2. Add a URL to urlpatterns: path('blog/', include('blog.urls'))
 """
 
-
 from django.contrib import admin
 from django.urls import include, path
 from django.conf.urls import handler403, handler404, handler500
