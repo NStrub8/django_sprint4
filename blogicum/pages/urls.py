73c8ec154063b5ed9b6c74053918375c59388df7
Nikita Strubalin <nikita.strubalin@yandex.ru>
2025-06-03 15:01:56 +0300

    8

--- blogicum/pages/urls.py
+++ blogicum/pages/urls.py
@@ -1,12 +1,11 @@
 from django.urls import path
-from .views import AboutPageView, RulesPageView
-from .views import edit_profile
+from .views import AboutPageView, RulesPageView, edit_profile, register
 
 app_name = 'pages'
 
 urlpatterns = [
     path('about/', AboutPageView.as_view(), name='about'),
     path('rules/', RulesPageView.as_view(), name='rules'),
-    path('registration/', views.register, name='register'),
-    path('edit_profile/', edit_profile, name='edit_profile'), 
+    path('registration/', register, name='register'),
+    path('edit_profile/', edit_profile, name='edit_profile'),
 ]
